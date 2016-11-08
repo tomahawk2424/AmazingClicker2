24e5407cce5be5779147853356533257831f2ff2
U012NXC <U012NXC@LWPHL1U012NXC>
2016-11-09 05:21:10 +0800

    Add project files.

---
+++ AmazingClicker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmazingClicker.Startup))]
+namespace AmazingClicker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
